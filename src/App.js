c014b6e92f52d46d57dbcf5a78b7eb835f267d79
David Green <dave@activelylazy.co.uk>
2017-09-15 17:44:06 +0100

    Setup basic nav header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Tabs, Tab } from './tabs';
 import logo from './logo.svg';
 import './App.css';
@@ -7,50 +7,59 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-6">
-              <Tabs>
-                <Tab title="Accounts">
-                  <div>Some content</div>
-                </Tab>
-                <Tab title="Categories">
-                  <div>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                    More content<br/>
-                  </div>
-                </Tab>
-              </Tabs>
+      <div>
+        <Navbar fixedTop='true'>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Mr. Budget</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">Accounts</NavItem>
+            <NavItem eventKey={2} href="#">Categories</NavItem>
+            <NavItem eventKey={3} href="#">Review</NavItem>
+            <NavItem eventKey={4} href="#">Import</NavItem>
+          </Nav>
+          <Navbar.Text pullRight>
+            Signed in as <u>Demo User</u>.
+          </Navbar.Text>
+        </Navbar>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+              More content<br/>
+            </div>
           </div>
         </div>
       </div>
