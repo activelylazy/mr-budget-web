94f50039e257a76d686e78ed2fca3d5ed91c69e9
David Green <dave@activelylazy.co.uk>
2017-09-27 17:49:38 +0100

    Style account selector

--- src/accounts/AccountSelector.jsx
+++ src/accounts/AccountSelector.jsx
@@ -4,8 +4,23 @@ import { Button } from 'react-bootstrap';
 
 const AccountsSelector = ({ account }) => (
   <div className="account">
-    <Button>
-      {account.name}
+    <Button
+      bsStyle="default"
+      block
+    >
+      <table className="account-selector-table">
+        <tr>
+          <td>{account.name}</td>
+          <td rowSpan="2" className="account-balance">
+            £ 123.45
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <span style={{ fontSize: '8pt' }}>last updated yesterday</span>
+          </td>
+        </tr>
+      </table>
     </Button>
   </div>
 );
