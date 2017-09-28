943635d33301aadd0c599e950e03f9073e6638f9
David Green <dave@activelylazy.co.uk>
2017-09-28 17:55:30 +0100

    Fix name of state to store

--- src/user-data/user-data-actions.js
+++ src/user-data/user-data-actions.js
@@ -59,5 +59,5 @@ export const loadUserData = () => (dispatch) => {
 
 export const addAccount = accountName => (dispatch, getState) => {
   dispatch(addAccountToState(accountName));
-  saveUserData(getState().accounts);
+  saveUserData(getState().userData);
 };
