355e78263e5601a04a99938eac5ff774d514c17c
David Green <dave@activelylazy.co.uk>
2017-10-06 20:44:36 +0100

    Add import to root reducer

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import navigation from './navigation/navigation-reducer';
 import userData from './user-data/user-data-reducer';
+import statementImport from './import/import-reducer';
 
 export default combineReducers({
   navigation,
   userData,
+  statementImport,
 });
