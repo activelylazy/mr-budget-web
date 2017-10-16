a871cada5fddafdacde5bb5523bbbb618f630d46
David Green <dave@activelylazy.co.uk>
2017-10-16 08:29:09 +0100

    Implement add transactions to month

--- src/financial-data/financial-data-reducer.js
+++ src/financial-data/financial-data-reducer.js
@@ -1,5 +1,5 @@
 import Immutable from 'seamless-immutable';
-import { FINANCIAL_DATA_LOADED } from './financial-data-actions';
+import { FINANCIAL_DATA_LOADED, APPLY_TRANSACTIONS_TO_MONTH } from './financial-data-actions';
 
 const defaultState = Immutable({
 });
@@ -8,11 +8,24 @@ function mergeYear(existingYear, month, financialData) {
   return existingYear.set(month, existingYear[month] || financialData);
 }
 
+function mergeMonthTransactions(existingMonth, transactions) {
+  return existingMonth.set('transactions',
+    existingMonth.transactions.concat(transactions),
+  );
+}
+
+function mergeYearTransactions(existingYear, month, transactions) {
+  return existingYear.set(month, mergeMonthTransactions(existingYear[month], transactions));
+}
+
 export default (state = defaultState, action) => {
   switch (action.type) {
     case FINANCIAL_DATA_LOADED:
       return state.set(action.year,
         mergeYear(state[action.year] || Immutable({}), action.month, action.financialData));
+    case APPLY_TRANSACTIONS_TO_MONTH:
+      return state.set(action.year,
+        mergeYearTransactions(state[action.year], action.month, action.transactions));
     default:
       return state;
   }
--- src/financial-data/financial-data-reducer.test.js
+++ src/financial-data/financial-data-reducer.test.js
@@ -1,7 +1,7 @@
 import { assert, should } from 'chai';
 import sinon from 'sinon';
 import Immutable from 'seamless-immutable';
-import { financialDataLoaded } from './financial-data-actions';
+import { financialDataLoaded, applyTransactionsToMonth } from './financial-data-actions';
 import financialDataReducer from './financial-data-reducer';
 
 should();
@@ -13,37 +13,106 @@ describe('financial data reducer', () => {
     assert(state.should.be.empty);
   });
 
-  it('handles financial data loaded by applying to state', () => {
-    const financialData = sinon.stub();
-    const state = financialDataReducer(undefined, financialDataLoaded(financialData, 2017, 7));
+  describe('handles financial data loaded', () => {
+    it('applies to empty state', () => {
+      const financialData = sinon.stub();
+      const state = financialDataReducer(undefined, financialDataLoaded(financialData, 2017, 7));
 
-    assert(state[2017][7].should.equal(financialData));
+      assert(state[2017][7].should.equal(financialData));
+    });
+
+    it('merges with unchanged year data', () => {
+      const originalData = sinon.stub();
+      const initialState = Immutable({
+        2016: {
+          5: originalData,
+        },
+      });
+      const financialData = sinon.stub();
+      const state = financialDataReducer(initialState, financialDataLoaded(financialData, 2017, 7));
+
+      assert(state[2016][5].should.equal(originalData));
+      assert(state[2017][7].should.equal(financialData));
+    });
+
+    it('preserves already loaded data for year + month', () => {
+      const originalData = sinon.stub();
+      const initialState = Immutable({
+        2017: {
+          7: originalData,
+        },
+      });
+      const financialData = sinon.stub();
+      const state = financialDataReducer(initialState, financialDataLoaded(financialData, 2017, 7));
+
+      assert(state[2017][7].should.equal(originalData));
+    });
   });
 
-  it('handles financial data loaded by merging with unchanged year data', () => {
-    const originalData = sinon.stub();
-    const initialState = Immutable({
-      2016: {
-        5: originalData,
-      },
+  describe('handles apply transactions to month', () => {
+    it('adds transactions to empty month', () => {
+      const initialState = Immutable({
+        2017: {
+          7: {
+            transactions: [],
+          },
+        },
+      });
+
+      const transactions = [
+        {
+          id: '1',
+          date: new Date(2017, 7, 1),
+          name: 'transaction 1',
+          amount: 12.34,
+        },
+      ];
+
+      const state = financialDataReducer(initialState,
+        applyTransactionsToMonth(2017, 7, transactions));
+
+      assert(state[2017][7].transactions.length.should.equal(1));
+      assert(state[2017][7].transactions[0].id.should.equal('1'));
+      assert(state[2017][7].transactions[0].name.should.equal('transaction 1'));
+      assert(state[2017][7].transactions[0].amount.should.equal(12.34));
     });
-    const financialData = sinon.stub();
-    const state = financialDataReducer(initialState, financialDataLoaded(financialData, 2017, 7));
 
-    assert(state[2016][5].should.equal(originalData));
-    assert(state[2017][7].should.equal(financialData));
-  });
+    it('appends transactions to existing month data', () => {
+      const initialState = Immutable({
+        2017: {
+          7: {
+            transactions: [
+              {
+                id: '0',
+                date: new Date(2017, 7, 1),
+                name: 'transaction 0',
+                amount: -11.11,
+              },
+            ],
+            closingBalances: sinon.stub(),
+          },
+        },
+      });
 
-  it('handles financial data loaded by keeping already loaded data for year & month', () => {
-    const originalData = sinon.stub();
-    const initialState = Immutable({
-      2017: {
-        7: originalData,
-      },
+      const transactions = [
+        {
+          id: '1',
+          date: new Date(2017, 7, 2),
+          name: 'transaction 1',
+          amount: 12.34,
+        },
+      ];
+
+      const state = financialDataReducer(initialState,
+        applyTransactionsToMonth(2017, 7, transactions));
+
+      assert(state[2017][7].transactions.length.should.equal(2));
+      assert(state[2017][7].transactions[0].id.should.equal('0'));
+      assert(state[2017][7].transactions[0].name.should.equal('transaction 0'));
+      assert(state[2017][7].transactions[1].id.should.equal('1'));
+      assert(state[2017][7].transactions[1].name.should.equal('transaction 1'));
+      assert(state[2017][7].transactions[1].amount.should.equal(12.34));
+      assert(state[2017][7].closingBalances.should.equal(initialState[2017][7].closingBalances));
     });
-    const financialData = sinon.stub();
-    const state = financialDataReducer(initialState, financialDataLoaded(financialData, 2017, 7));
-
-    assert(state[2017][7].should.equal(originalData));
   });
 });
