1683faaf2d74499850a1403d762a9321983d43d5
David Green <dave@activelylazy.co.uk>
2017-10-16 08:05:50 +0100

    Add financial data to root reducer

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import navigation from './navigation/navigation-reducer';
 import userData from './user-data/user-data-reducer';
 import statementImport from './import/import-reducer';
+import financialData from './financial-data/financial-data-reducer';
 
 export default combineReducers({
   navigation,
   userData,
   statementImport,
+  financialData,
 });
