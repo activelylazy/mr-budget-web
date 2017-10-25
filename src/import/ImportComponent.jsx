bfe7e7d8ab5e05baa285d8f0fbae1dc75b83eaba
David Green <dave@activelylazy.co.uk>
2017-10-25 17:58:32 +0100

    Remove unused requirement

--- src/import/ImportComponent.jsx
+++ src/import/ImportComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Grid, Row, Col, Panel, Glyphicon, Button } from 'react-bootstrap';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
