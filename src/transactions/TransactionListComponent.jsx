f2270f71f2e81180525c32e703c18e8bdd19762e
David Green <dave@activelylazy.co.uk>
2017-11-15 07:51:46 +0000

    Pass running account balance to transaction component

--- src/transactions/TransactionListComponent.jsx
+++ src/transactions/TransactionListComponent.jsx
@@ -2,21 +2,35 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import Transaction from './TransactionComponent';
 
-const TransactionListComponent = ({ transactions }) => (
-  <div>
-    {transactions.map(transaction => (
-      <Transaction
-        key={transaction.id}
-        transaction={transaction}
-      />))}
-  </div>
-);
+const TransactionListComponent = ({ transactions, openingBalance }) => {
+  let balance = openingBalance;
+  const transactionElements = [];
+  transactions.forEach((transaction) => {
+    balance += transaction.amount;
+    transactionElements.push(<Transaction
+      key={transaction.id}
+      transaction={transaction}
+      balance={balance}
+    />);
+  });
+
+  return (
+    <div>
+      {transactionElements}
+    </div>
+  );
+};
 
 TransactionListComponent.propTypes = {
   transactions: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
     amount: PropTypes.number.isRequired,
   })).isRequired,
+  openingBalance: PropTypes.number,
+};
+
+TransactionListComponent.defaultProps = {
+  openingBalance: undefined,
 };
 
 export default TransactionListComponent;
--- src/transactions/TransactionListComponent.test.jsx
+++ src/transactions/TransactionListComponent.test.jsx
@@ -31,4 +31,30 @@ describe('transaction list component', () => {
     assert(transactionComponents.get(0).props.transaction.should.equal(transaction1));
     assert(transactionComponents.get(1).props.transaction.should.equal(transaction2));
   });
+
+  it('passes account balance', () => {
+    const transaction1 = {
+      id: '1',
+      name: 'transaction 1',
+      amount: 12.34,
+    };
+    const transaction2 = {
+      id: '2',
+      name: 'transaction 2',
+      amount: 45.67,
+    };
+    const transactions = [transaction1, transaction2];
+    const transactionList = shallow(
+      <TransactionList
+        transactions={transactions}
+        openingBalance={100}
+      />,
+    );
+
+    const transactionComponents = transactionList.find(Transaction);
+
+    assert(transactionComponents.length.should.equal(2));
+    assert(transactionComponents.get(0).props.balance.should.equal(112.34));
+    assert(transactionComponents.get(1).props.balance.should.equal(158.01));
+  });
 });
