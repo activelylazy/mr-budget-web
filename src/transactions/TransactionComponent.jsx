dc823d323b03e0e5bf97d93fa955823b318bd658
David Green <dave@activelylazy.co.uk>
2017-11-15 08:24:40 +0000

    Render balance if one is provided

--- src/transactions/TransactionComponent.jsx
+++ src/transactions/TransactionComponent.jsx
@@ -3,28 +3,41 @@ import { PropTypes } from 'prop-types';
 import moment from 'moment';
 import './transactions.css';
 
-const TransactionComponent = ({ transaction }) => (
-  <div className="transaction">
-    <table>
-      <tbody>
-        <tr>
-          <td rowSpan="2" className="date">{moment(transaction.date).format('DD/MM/YYYY')}</td>
-          <td className="desc">{transaction.name}</td>
-          <td rowSpan="2" className="amount">£ {transaction.amount.toFixed(2)}</td>
-        </tr>
-        <tr>
-          <td className="category">A category</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+const TransactionComponent = ({ transaction, balance }) => {
+  const cells = [];
+  if (balance !== undefined) {
+    cells.push(<td key="balance" rowSpan="2" className="balance">£ {balance.toFixed(2)}</td>);
+  }
+  return (
+    <div className="transaction">
+      <table>
+        <tbody>
+          <tr>
+            <td rowSpan="2" className="date">{moment(transaction.date).format('DD/MM/YYYY')}</td>
+            <td className="name">{transaction.name}</td>
+            <td rowSpan="2" className="amount">£ {transaction.amount.toFixed(2)}</td>
+            {cells}
+          </tr>
+          <tr>
+            <td className="category">A category</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 TransactionComponent.propTypes = {
   transaction: PropTypes.shape({
+    date: PropTypes.instanceOf(Date),
     name: PropTypes.string.isRequired,
     amount: PropTypes.number.isRequired,
   }).isRequired,
+  balance: PropTypes.number,
+};
+
+TransactionComponent.defaultProps = {
+  balance: undefined,
 };
 
 export default TransactionComponent;
--- src/transactions/TransactionComponent.test.jsx
+++ src/transactions/TransactionComponent.test.jsx
@@ -16,4 +16,48 @@ describe('transaction component', () => {
 
     assert(component.find('.date').text().should.equal('01/08/2017'));
   });
+
+  it('renders name', () => {
+    const transaction = {
+      name: 'the transaction',
+      amount: 12.34,
+      date: new Date(2017, 7, 1),
+    };
+    const component = shallow(<Transaction transaction={transaction} />);
+
+    assert(component.find('.name').text().should.equal('the transaction'));
+  });
+
+  it('renders amount', () => {
+    const transaction = {
+      name: 'the transaction',
+      amount: 12.34,
+      date: new Date(2017, 7, 1),
+    };
+    const component = shallow(<Transaction transaction={transaction} />);
+
+    assert(component.find('.amount').text().should.equal('£ 12.34'));
+  });
+
+  it('does not render balance if none present', () => {
+    const transaction = {
+      name: 'the transaction',
+      amount: 12.34,
+      date: new Date(2017, 7, 1),
+    };
+    const component = shallow(<Transaction transaction={transaction} />);
+
+    assert(component.find('.balance').exists().should.equal(false));
+  });
+
+  it('renders balance if present', () => {
+    const transaction = {
+      name: 'the transaction',
+      amount: 12.34,
+      date: new Date(2017, 7, 1),
+    };
+    const component = shallow(<Transaction transaction={transaction} balance={22.33} />);
+
+    assert(component.find('.balance').text().should.equal('£ 22.33'));
+  });
 });
